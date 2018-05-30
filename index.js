1b6a3772ef04677c3034844438ccde71c344b9fc
DESKTOP-ADG351T\Nacabacu <punnclub@hotmail.com>
2018-05-30 18:41:17 +0700

    Add base62 class

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const Base62 = require('./base62');
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+  var base62 = new Base62();
+  response.send(base62.encode(123))
 })
 
 app.listen(port, (err) => {
