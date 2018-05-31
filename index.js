9574f83748c5d910db453646bf602e004105f6d4
DESKTOP-ADG351T\Nacabacu <punnclub@hotmail.com>
2018-05-31 11:10:49 +0700

    Change Base62 lib instead of Base62 class

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
-const Base62 = require('./base62');
+const Base62 = require('base62');
 const request = require('request');
 
 const app = express();
@@ -9,8 +9,7 @@ const port = 3000;
 app.use(express.json());
 
 app.post('/getBase62', (req, res) => {
-  var base62 = new Base62();
-  res.send({ answer: base62.decode(req.body.text) });
+  res.send({ answer: Base62.decode(req.body.text) });
 });
 
 app.post('/getMd5', (req, res) => {
@@ -24,7 +23,6 @@ app.get('/mockGetTask', (req, response) => {
     }
 
     var resObject = JSON.parse(res.body);
-    var base62 = new Base62();
     var start = resObject.start;
     var end = resObject.end;
     var hashResult = resObject.hashes;
@@ -32,12 +30,11 @@ app.get('/mockGetTask', (req, response) => {
 
     if (resObject.newTask) {
       for (var i = start; i <= end; i++) {
-        var text = base62.encode(i);
+        var text = Base62.encode(i);
         var hash = crypto.createHash('md5').update(text).digest('hex');
 
         if (hash == hashResult) {
           found = true;
-          console.log(text)
           response.send(text);
         }
       }
